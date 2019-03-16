b1578014f5d88e325aef311e96a5bd605b576067
Jack Zhang <ngshoy@hotmail.com>
2019-03-16 14:21:54 -0400

    init plans and subscriptions service

--- src/controllers/plans-controller.js
+++ src/controllers/plans-controller.js
@@ -1,20 +1,31 @@
 const router = require('express').Router;
 const asyncWrapper = require('../utils/async-wrapper').AsyncWrapper;
+const PlansService = require("../services/plans-service");
+
+const plansService = new PlansService();
 
 router.get('/', asyncWrapper(async (req, res) => {
-
+  const userId = null;
+  const plans = await plansService.findAll(userId);
+  res.send(plans);
 }));
 
 router.get('/:id', asyncWrapper(async (req, res) => {
-
+  const id = req.params.id;
+  const userId = null;
+  const plan = await plansService.findOne(id);
+  res.send(plan);
 }));
 
 router.post('/', asyncWrapper(async (req, res) => {
-
+  const plan = await plansService.create(req.body);
+  res.send(plan);
 }));
 
 router.delete('/:id', asyncWrapper(async (req, res) => {
-
+  const id = req.params.id;
+  await plansService.deleteOne(id);
+  res.sendStatus(200);
 }));
 
 module.exports = router;
--- src/controllers/subscription-controller.js
+++ src/controllers/subscription-controller.js
@@ -1,20 +1,31 @@
 const router = require('express').Router;
 const asyncWrapper = require('../utils/async-wrapper').AsyncWrapper;
+const SubscriptionsService = require("../services/subscriptions-service");
+
+const subscriptionsService = new SubscriptionsService();
 
 router.get('/', asyncWrapper(async (req, res) => {
-
+  const userId = null;
+  const subscription = await subscriptionsService.findAll(userId);
+  res.send(subscription);
 }));
 
 router.get('/:id', asyncWrapper(async (req, res) => {
-
+  const id = req.params.id;
+  const userId = null;
+  const plan = await subscriptionsService.findOne(id);
+  res.send(plan);
 }));
 
 router.post('/', asyncWrapper(async (req, res) => {
-
+  const plan = await subscriptionsService.create(req.body);
+  res.send(plan);
 }));
 
 router.delete('/:id', asyncWrapper(async (req, res) => {
-
+  const id = req.params.id;
+  await subscriptionsService.deleteOne(id);
+  res.sendStatus(200);
 }));
 
 module.exports = router;
