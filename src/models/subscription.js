c56ef4af346eddd3583da8ef3cc9c686478fb070
Jack Zhang <ngshoy@hotmail.com>
2019-03-18 21:45:46 -0400

    added plan and subscription models

--- src/models/subscription.js
+++ src/models/subscription.js
@@ -1,5 +1,21 @@
 const Joi = require('joi');
 
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Subscription = sequelize.define('Subscription', {
+    planId: DataTypes.INTEGER,
+    coupon: DataTypes.STRING,
+    cardNumber: DataTypes.STRING,
+    holderName: DataTypes.STRING,
+    expirationDate: DataTypes.STRING,
+    cvv: DataTypes.STRING
+  }, {});
+  Subscription.associate = function(models) {
+    // associations can be defined here
+  };
+  return Subscription;
+};
+
 module.exports.SubscriptionValidationSchema = Joi.object().keys({
   planId: Joi.number().positive().required(),
   coupon: Joi.number().min(0).max(100).optional().allow(null),
