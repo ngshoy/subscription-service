7cd10062d59e464229e01428485d329fbe825b1c
Jack Zhang <ngshoy@hotmail.com>
2019-03-19 22:07:37 -0400

    add migration scripts to populate tables

--- src/config/config.js
+++ src/config/config.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 if (!process.env.MYSQL_HOST) {
   dotenv.config({
-    path: path.join(__dirname, '..', '.env')
+    path: path.join(__dirname, '../..', '.env')
   })
 }
 
@@ -13,5 +13,5 @@ module.exports = {
   password: process.env.MYSQL_PASS,
   port: process.env.MYSQL_PORT,
   database: process.env.MYSQL_DB,
-  dialect: 'mysql q'
+  dialect: 'mysql'
 };
