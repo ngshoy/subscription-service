009ada318888c79a7b248885849006c668ca60b8
Jack Zhang <ngshoy@hotmail.com>
2019-03-25 21:09:16 -0400

    initiating auth-service

---
+++ src/auth-service/models/user.js
@@ -0,0 +1,22 @@
+const Joi = require('joi');
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
+
+module.exports.UserValidationSchema = Joi.object().keys({
+  firstName: Joi.string().required(),
+  lastName: Joi.string().required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().required.min(6).max(32)
+});
